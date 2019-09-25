b5811c6bdc9828e9554bbbf304288b2d25e3efbf
Daniel Crewdson <daniel.crewdson@dewsign.co.uk>
2019-09-25 22:33:25 +0100

    init cus

---
+++ wall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+category=$1
+url="https://source.unsplash.com/6000x4000/?"
+
+image_url="${url}${category}"
+
+image=$(wget --output-document=image.png ${image_url})
+
+ gsettings set org.gnome.desktop.background picture-uri $PWD/image.png
