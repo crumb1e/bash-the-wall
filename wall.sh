6c9cbcecb148b9b2faaa5105f919de775078d0d5
Daniel Crewdson <daniel.crewdson@dewsign.co.uk>
2020-05-29 13:53:18 +0100

    split functionality into functions - much cleaner

--- wall.sh
+++ wall.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+while getopts ":s:" opt; do
+    case $opt in
+        s) cp ~/.wallpaper/image.jpeg ~/.wallpaper/saved/$OPTARG.jpeg 
+        exit 1
+    esac
+done
+
 category=$1
 categories=$@
 url="https://source.unsplash.com/6000x4000/?"
@@ -11,33 +18,42 @@ for i in ${categories}; do
     comma=","
 done
 
-if [ ! -d "~/.wallpaper" ]
-then
-    echo "No wallpaper directory found, creating one now!"
-    mkdir ~/.wallpaper
-fi
-
 # functions
 command_exists () {
     type "$1" &> /dev/null ;
+}
+
+mac_check_dir () {
+    if [ ! -d "/Users/$(whoami)/.wallpaper" ]; then
+        echo "No wallpaper directory found, creating one now!"
+        mkdir ~/.wallpaper
+        mkdir ~/.wallpaper/saved
+    fi
 }
 
 mac_wallpaper () {
     osascript -e 'tell application "System Events" to tell every desktop to set picture to "~/.wallpaper/image.jpeg"'
     killall Dock
 }
+
+get_wallpaper () {
+    image_url="${url}"
+
+    wget ${image_url} -O ~/.wallpaper/temp_image.jpeg &&
+
+    mv ~/.wallpaper/temp_image.jpeg ~/.wallpaper/image.jpeg &&
+    rm -rf ~/.wallpaper/temp_image.jpeg
+}
 # end functinos
 
-image_url="${url}"
 
-wget ${image_url} -O ~/.wallpaper/temp_image.jpeg &&
-
-mv ~/.wallpaper/temp_image.jpeg ~/.wallpaper/image.jpeg &&
-rm -rf ~/.wallpaper/temp_image.jpeg
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
+    mac_check_dir
+    get_wallpaper
     mac_wallpaper ~/.wallpaper/image.jpeg
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+    get_wallpaper
     if command_exists feh ; then
         feh --bg-scale ~/.wallpaper/image.jpeg
     fi
